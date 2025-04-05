edd1eb1b5021d2dfa0134d4d201e324288f3687c
rahul48635 <rahulsingh48635@gmail.com>
2025-04-05 10:22:58 +0530

    added comp library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import './App.css'
 function App() {
+  const [mnemonic, setMnemonic] = useState("");
   return (
     <div className="bg-blue-500 text-black justify-center items-center">
         
